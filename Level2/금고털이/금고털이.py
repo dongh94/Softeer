b1aea3c55489fc52ad3b7a55a3e36925b83aa5d0
Donghee <dongh94@naver.com>
2023-01-26 14:21:01 +0900

    금고털이 확인

--- Level2/금고털이/금고털이.py
+++ Level2/금고털이/금고털이.py
@@ -1,1 +1,27 @@
-import sys; sys.
+import sys;
+sys.stdin = open('금고털이.txt')
+
+W, N = map(int, input().split())
+arr = []
+for _ in range(N):
+    m, p = map(int, input().split())
+    arr.append((m,p))
+
+s_arr = sorted(arr, key=lambda x: x[1], reverse=True)
+answer = 0
+for i in range(N):
+    if W >= s_arr[i][0]:
+        W -= s_arr[i][0]
+        answer += s_arr[i][0] * s_arr[i][1]
+
+    elif W < s_arr[i][0]:
+        answer += W * s_arr[i][1]
+        break
+
+print(answer)
+
+
+
+
+
+
